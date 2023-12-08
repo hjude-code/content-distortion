13efdd22810ca02e3b7883d715b315098bde82f7
Harrison Jude <harrison@hjude.com>
2023-12-08 11:16:16 -0500

    added basic clipping

--- build/render.php
+++ build/render.php
@@ -4,10 +4,16 @@
  */
 ?>
 <div <?php echo get_block_wrapper_attributes(); ?>>
-	<?php echo $content ?>
+	<?php
+		echo "<div class='itterationLayer itterationBase' aria-hidden='true'>";
+		echo $content;
+		echo "</div>";
+	?>
 	<?php
 		for ($x = 0; $x < $attributes['itterations']; $x++){
+			echo "<div class='itterationLayer' aria-hidden='true'>";
 			echo $content;
+			echo "</div>";
 		};
 	?>
 
